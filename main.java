3ce2501affa313bb19eef6675899c8d2c34b411a
Felipe Sobral <fsobral3106@gmail.com>
2026-03-30 11:48:26 -0300

    Correção da lógica de status e finalização do projeto

--- main.java
+++ main.java
@@ -1,3 +1,19 @@
-public class main {
+public class Main {
+    public static void main(String[] args) {
+        Livro livro1 = new Livro("O Senhor dos Anéis", "J.R.R. Tolkien");
     
+        System.out.println("=== bliblioteca ===");
+        System.out.println("Livro: " + livro1.getTitulo());
+        System.out.println("Autor: " + livro1.getAutor());
+        System.out.println("Status: " + (livro1.isEstaEmprestado() ? "Emprestado" : "Disponível"));
+
+        //simulando o emprestimo do livro
+        System.out.println("\nRealizando o empréstimo de:" + livro1.getTitulo());
+        livro1.emprestar();
+
+        //verificando o status do livro após o empréstimo
+        System.out.println("Status: " + (livro1.isEstaEmprestado()  ? "Emprestado" : "Disponível"));
+
+        System.out.println("=========================================");
+    }
 }
